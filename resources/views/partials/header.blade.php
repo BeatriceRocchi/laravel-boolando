5e58e7a90e0d603c59c4079771057f8acd42b7a2
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-05-09 14:42:55 +0200

    [add] header style and content with static menues

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,23 @@
-<header>Header</header>
+<header>
+    <div class="container">
+        <nav>
+            <ul class="menu">
+                <li>
+                    <a href="#">Donna</a>
+                </li>
+                <li>
+                    <a href="#">Uomo</a>
+                </li>
+                <li>
+                    <a href="#">Bambino</a>
+                </li>
+            </ul>
+        </nav>
+        <img src="/img/boolean-logo.png" alt="Logo boolean" />
+        <div>
+            <i class="fa-regular fa-user"></i>
+            <i class="fa-regular fa-heart"></i>
+            <i class="fa-solid fa-bag-shopping"></i>
+        </div>
+    </div>
+</header>
