1ff470035baf2e51f147d3476b9ab93ba5251e91
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-05-09 15:37:42 +0200

    [add] footer style and content with static menues, [add] products db in config

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.main')
 
 @section('content')
-    <div>Main</div>
+    <main>
+        <div class="container">
+            @foreach ($products as $product)
+                <div>{{ $product['brand'] }}</div>
+                {{-- <div class="card">
+                    <div class="top-card">
+                        <img class="default-img" :src="{{Vite::assets('resources/img/$product['frontImage']')}}" :alt="{{$product['brand']}}" />
+                        <img class="hover-img" :src="{{Vite::assets('resources/img/$product['backImage']')}}" :alt="{{$product['brand']}}" />
+                        <div class="wish" :class="{ 'in-wishlist': productObject.isInFavorites }">
+                            &hearts;
+                        </div>
+                        <div class="badges-box">
+                            <span v-for="(badge, index) in productObject.badges" :key="index"
+                                :class="badge.type">
+                                {{ badge . value }}</span>
+                        </div>
+                    </div>
+                    <div class="bottom-card">
+                        <div class="brand">{{ productObject . brand }}</div>
+                        <div class="product">{{ productObject . name }}</div>
+                        <div>
+                            <span v-if="calcSalesPrice()" class="discounted-price">{{ calcSalesPrice() }} &euro;
+                            </span>
+                            <span class="full-price" :class="{ strike: calcSalesPrice() }">
+                                {{ productObject . price }} &euro;</span>
+                        </div>
+                    </div>
+                </div> --}}
+            @endforeach
+
+        </div>
+    </main>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,24 @@
-<footer>Footer</footer>
+<footer>
+    <div class="container">
+        <div>
+            <h4>Boolando s.r.l</h4>
+            <nav>
+                <ul class="references">
+                    <li><a href="#">Informazioni legali</a></li>
+                    <li><a href="#">Informativa sulla privacy</a></li>
+                    <li><a href="#">Diritto di recesso</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div>
+            <h4>Trovaci anche su</h4>
+            <div class="icons">
+                <a href="#"><i class="fa-brands fa-square-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-square-pinterest"></i></a>
+                <a href="#"><i class="fa-brands fa-square-youtube"></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
